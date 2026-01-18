00a7957608b1d92849f42c567f96d9610ad15f57
Ignacio Hagopian <jsign.uy@gmail.com>
2026-01-18 07:33:08 -0300

    use same prep as reth

--- crates/integration-tests/tests/stateless-validator-ethrex.rs
+++ crates/integration-tests/tests/stateless-validator-ethrex.rs
@@ -2,19 +2,36 @@
 
 use ere_dockerized::zkVMKind;
 use guest::Guest;
-use integration_tests::{NoopPlatform, TestCase, get_fixtures};
+use integration_tests::{
+    NoopPlatform, TestCase, get_fixtures, stateless_validator::get_stateless_validator_output,
+};
 use stateless_validator_ethrex::guest::{
     StatelessValidatorEthrexGuest, StatelessValidatorEthrexInput,
 };
+use stateless_validator_reth::guest::{StatelessValidatorRethGuest, StatelessValidatorRethInput};
 
 fn test_execution(zkvm_kind: zkVMKind) {
     let fixtures = get_fixtures();
     let inputs = fixtures.into_iter().map(|fixture| {
-        let input =
-            StatelessValidatorEthrexInput::new(&fixture.stateless_input, fixture.success).unwrap();
-        let output = StatelessValidatorEthrexGuest::compute::<NoopPlatform>(input.clone());
+        let output = if !fixture.success {
+            let input = StatelessValidatorRethInput::new(&fixture.stateless_input, fixture.success)
+                .unwrap();
+            // For invalid blocks we can't correctly generate the NewPayloadRequest
+            // from an EL block. This is because to get the Electra requests, we
+            // need to execute the block successfully first.
+            StatelessValidatorRethGuest::compute::<NoopPlatform>(input.clone())
+        } else {
+            // For valid blocks (i.e. mainnet), we can rely on testing the output against an independent
+            // implementation that calculated the NewPayloadRequest root from a CL block.
+            get_stateless_validator_output(
+                fixture.stateless_input.block.hash_slow(),
+                fixture.success,
+            )
+        };
         assert_eq!(output.successful_block_validation, fixture.success);
 
+        let input =
+            StatelessValidatorEthrexInput::new(&fixture.stateless_input, fixture.success).unwrap();
         TestCase::new::<StatelessValidatorEthrexGuest>(fixture.name, input, output).output_sha256()
     });
     integration_tests::test_execution("stateless-validator-ethrex", zkvm_kind, inputs);
